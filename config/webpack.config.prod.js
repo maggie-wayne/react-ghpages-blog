02ae163552695861cf82b6b8397ad75e7ecaf29d
zowiegong <zowiegong@gmail.com>
2018-06-19 17:40:10 +0800

    fix #7

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -168,7 +168,7 @@ module.exports = {
                     // use the "style" loader inside the async code so CSS from them won't be
                     // in the main CSS file.
                     {
-                        test: /\.css$/,
+                        test: /\.(css|less)$/,
                         loader: ExtractTextPlugin.extract(
                             Object.assign(
                                 {
@@ -207,6 +207,9 @@ module.exports = {
                                                 ],
                                             },
                                         },
+                                        {
+                                            loader: require.resolve('less-loader') // compiles Less to CSS
+                                        }
                                     ],
                                 },
                                 extractTextPluginOptions

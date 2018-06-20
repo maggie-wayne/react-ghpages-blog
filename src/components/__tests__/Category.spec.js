159fc13dc0814e374076ce9181101801908f44c0
zowiegong <zowiegong@gmail.com>
2018-06-20 10:47:32 +0800

    update

--- src/components/__tests__/Category.spec.js
+++ src/components/__tests__/Category.spec.js
@@ -3,7 +3,7 @@ import { mount } from 'enzyme';
 import { MemoryRouter } from 'react-router-dom'
 
 import Category from '../Category'
-import { capitalizeFirstLetter } from '../utils'
+import { capitalizeFirstLetter } from '../../utils'
 
 const MockData = JSON.parse(`{"name":"privacy","path":"posts/privacy","sha":"e09fa8bb3f396f31af120858d0ebea23e0a4750d","size":0,"url":"https://api.github.com/repos/zowiegong/blog/contents/posts/privacy?ref=master","htmlUrl":"https://github.com/zowiegong/blog/tree/master/posts/privacy","gitUrl":"https://api.github.com/repos/zowiegong/blog/git/trees/e09fa8bb3f396f31af120858d0ebea23e0a4750d","downloadUrl":null,"type":"dir","links":{"self":"https://api.github.com/repos/zowiegong/blog/contents/posts/privacy?ref=master","git":"https://api.github.com/repos/zowiegong/blog/git/trees/e09fa8bb3f396f31af120858d0ebea23e0a4750d","html":"https://github.com/zowiegong/blog/tree/master/posts/privacy"}}`)
 

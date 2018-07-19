6726a7a2b356b7a2ecd9fe1b706fced4858011b2
zowiegong <zowiegong@gmail.com>
2018-07-19 21:34:34 +0800

    update config

--- src/config.js
+++ src/config.js
@@ -4,13 +4,14 @@ module.exports = {
     author: 'zowiegong',
     keywords: 'bolg, web, javascript, react, zowiegong, 博客',
 
-    domain: 'http://localhost:3000',
+    domain: 'https://zowiegong.com',
 
     baseURL: 'https://api.github.com',
     requestTimeOut: 20 * 1000,
 
     // 开启评论需修改 /public/oauth.html 的 client_id、client_secret 值，从 https://github.com/settings/applications/new 注册获取
     comment: true,
+    client_id: '8622cc9adc195017b14d',
 
     owner: 'zowiegong',
     repo: 'blog',
